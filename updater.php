7be76363974b660e51ed763a9b7e6b004b3fd1ee
Lukas Danuser <l.danuser@rafisa.ch>
2022-05-18 14:31:41 +0200

    minor changes

--- updater.php
+++ updater.php
@@ -122,8 +122,8 @@ class Courses_Updater
         $result['destination'] = $install_directory; // Set the destination for the rest of the stack
 
         if ($this->active) { // If it was active
-
-            activate_plugin($this->basename); // Reactivate
+            Inc\Base\Activate::activate();
+            //activate_plugin($this->basename); // Reactivate
         }
         return $result;
     }
