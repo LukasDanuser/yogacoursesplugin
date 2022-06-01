a705b3bd4a27a632dce5c948cb616ea16c37113e
Lukas Danuser <l.danuser@rafisa.ch>
2022-06-01 10:10:47 +0200

    updated video display

--- Inc/Base/snippets/videos.php
+++ Inc/Base/snippets/videos.php
@@ -22,6 +22,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 global $wpdb;
+$plugin_data = get_plugin_data(dirname(__FILE__, 2) . '/coursesplugin/courses-plugin.php');
+$plugin_version = $plugin_data['Version'];
 wp_enqueue_style('style', '/wp-content/plugins/coursesplugin/assets/style.css', __FILE__, $plugin_version);
 
 if (is_user_logged_in()) {
@@ -31,40 +33,54 @@ if (is_user_logged_in()) {
     $date = date("Y-m-d");
     if ($valid_until != "0000-00-00" and $membership != "0") {
         if ($date > $valid_until) {
+            echo "<div class=\"accessDenied\" style=\"text-align: center;\">";
             echo "Diese Seite ist nur für Mitglieder zugänglich!\nSie können <a href=\"/membership\">hier</a> eine Mitgliedschaft kaufen.";
+            echo "</div>";
         } else {
+            $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
             $videos = $wpdb->get_results("SELECT * FROM $wpdb->prefix" . "courseVideos");
+            $rowsNeeded = ceil(sizeof($videos) / 4);
+            $courseAmount = sizeof($videos);
+            $columnsNeeded = $courseAmount >= 4 ? 4 : 3;
+            $columnsNeeded = $courseAmount == 2 ? 2 : $columnsNeeded;
+            $centerThree = $courseAmount == 1 ? true : false;
+            $maxWidth = ($courseAmount == 2) ? "50%" : (($courseAmount == 3)  ? "75%" : "100%");
             if (!empty($videos)) {
-                $t1Video = 0;
-                $t2Video = 1;
-                echo "<div class=\"videos\" style=\"display: grid; grid-template-columns: repeat(5, minmax(20em, 1fr)); grid-gap: 1rem; grid-template-rows: repeat(auto-fill, minmax(20em, 1fr));\">";
+                $t1 = 0;
+                $t2 = 1;
+                echo "<style>.wp-container-7>* {
+                    max-width: $maxWidth !important;
+                }</style>";
+                echo "<div class=\"videos\" style=\"display: grid; grid-template-columns: repeat($columnsNeeded, 1fr); column-gap: 1rem; row-gap: 1rem; grid-template-rows: repeat($rowsNeeded, 1fr);\">";
                 foreach ($videos as $video) {
-                    $t1Video++;
-                    if ($t1Video > 5) {
-                        $t2Video++;
-                        $t1Video = 1;
+                    $t1++;
+                    if ($t1 > $columnsNeeded) {
+                        $t2++;
+                        $t1 = 1;
                     }
+                    $t1 = $centerThree == true ? 2 : $t1;
                     echo "
-                  <div class=\"video\" style=\"grid-column: $t1Video; grid-row: $t2Video;\">
-                  <div id=\"video$row->id\" class=\"video-card\">
-                      <div class=\"video-body\">
-                          <h5 class=\"video-title text-center\"><b><u>
-                                      <p>$video->video_name</p>
-                                  </u></b></h5>
-                          <p class=\"video-text\">$video->video_description</p>
-                          <video width=\"320\" height=\"240\" controls>
-                    <source src=\"$video->file_url\" type=\"video/mp4\">
-                    Your browser does not support the video tag.
-                    </video> <br><br>
-                          <div class=\"Löschen text-center\">
-                              <button onclick=\"window.location.href='$actual_link&deleteVid=$video->id';\">Löschen</button>
-                          </div>
-                      </div>
-                  </div>
-            </div>";
+                    <div class=\"video\" style=\"grid-column: $t1; grid-row: $t2; margin-left: auto !important; margin-right: auto !important; max-width: 30rem !important;\">
+                        <div id=\"video$video->id\" class=\"video-card\">
+                            <div class=\"video-body\">
+                                <h5 class=\"video-title text-center\"><b><u>
+                                 <p>$video->video_name</p>
+                                </u></b></h5>
+                                <p class=\"video-text\">$video->video_description</p>
+                                <video width=\"320\" height=\"240\" controls>
+                                    <source src=\"$video->file_url\" type=\"video/mp4\">
+                                    Your browser does not support the video tag.
+                                </video> <br>
+                            </div>
+                        </div>
+                    </div>";
                 }
                 echo "</div>";
             }
         }
+    } else {
+        echo "<div class=\"accessDenied\" style=\"text-align: center;\">";
+        echo "Diese Seite ist nur für Mitglieder zugänglich!\nSie können <a href=\"/membership\">hier</a> eine Mitgliedschaft kaufen.";
+        echo "</div>";
     }
 }
