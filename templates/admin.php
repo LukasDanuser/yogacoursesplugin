7e4a26259f2d0223ab6c6367f6bde077673d4640
Lukas Danuser <l.danuser@rafisa.ch>
2022-06-07 16:13:48 +0200

    minor changes in comments

--- templates/admin.php
+++ templates/admin.php
@@ -4,7 +4,7 @@
  * @package CoursesPlugin
  */
 
-/*https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous
+/*
 Copyright (C) 2021  Rafisa Informatik GmbH
 
 This program is free software: you can redistribute it and/or modify
