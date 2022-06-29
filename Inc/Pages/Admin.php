e977a1f5226347a9f15de2241d006ce78680c074
Lukas Danuser <l.danuser@rafisa.ch>
2022-06-29 09:46:18 +0200

    minor changes

--- Inc/Pages/Admin.php
+++ Inc/Pages/Admin.php
@@ -22,7 +22,7 @@ class Admin extends BaseController
     public function add_admin_pages()
     {
         $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[SERVER_NAME]";
-        add_menu_page('Courses Plugin', 'Courses', 'manage_options', 'courses_plugin', array($this, 'admin_index'), 'dashicons-welcome-learn-more', '58');
+        add_menu_page('Kurse Admin', 'Kurse', 'manage_options', 'courses_plugin', array($this, 'admin_index'), 'dashicons-welcome-learn-more', '58');
         add_submenu_page(null, 'Courses Plugin', null, 'manage_options', 'upload_file', array($this, 'admin_upload_index'), '59');
     }
 
