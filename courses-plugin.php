01d7d30f198025240de224b2320beff3d711d09c
Lukas Danuser <l.danuser@rafisa.ch>
2022-08-08 14:00:30 +0200

    version 1.4.2

--- courses-plugin.php
+++ courses-plugin.php
@@ -7,7 +7,7 @@
 /*
 Plugin Name: Courses Plugin
 Description: Create and sell online courses.
-Version: 1.4.1
+Version: 1.4.2
 Author: Lukas Danuser
 License: GPLv3 or later
 Text Domain: courses-plugin
