67d83a982d7582bc1d6de69f8a59f32997498ec9
Lukas Danuser <lukas.danuser@gmail.com>
2023-04-17 00:41:23 +0200

    updated account tab

--- Inc/Base/snippets/accounttab.php
+++ Inc/Base/snippets/accounttab.php
@@ -4,10 +4,20 @@ function uwp_account_available_tabs_cb($tabs)
 {
     unset($tabs['notifications']);
     unset($tabs['privacy']);
+    unset($tabs['account']);
+    unset($tabs['change-password']);
 
+    $tabs['account'] = array(
+        'title' => __('Konto', 'userswp'),
+        'icon'  => 'fas fa-user mr-1 fa-fw',
+    );
     $tabs['courses'] = array(
         'title' => __('Meine Kurse', 'userswp'),
         'icon'  => 'fas fa-graduation-cap',
+    );
+    $tabs['change-password'] = array(
+        'title' => __('Passwort ändern', 'userswp'),
+        'icon'  => 'fas fa-asterisk mr-1 fa-fw',
     );
     return $tabs;
 }
@@ -53,6 +63,50 @@ function uwp_account_form_display_cb($type)
         } else {
             $columnsNeeded = 2;
         }
+        $autooptin = wp_get_current_user()->autooptin;
+        $autooptinnew = $autooptin == 0 ? 1 : 0;
+        $autooptinbutton = $autooptin == 0 ? 'Automatisches anmelden einschalten' : 'Automatisches anmelden ausschalten';
+
+        if (isset($_GET['autooptin'])) {
+            global $wpdb;
+            $userID = wp_get_current_user()->ID;
+            $table = $wpdb->prefix . 'users';
+            $data = array('autooptin' => $autooptinnew);
+            $where = array('ID' => $userID);
+            $wpdb->update($table, $data, $where);
+            echo " <script>window.location.replace(\"/account?type=courses\");</script>";
+        }
+        echo "<a style=\"text-align: center;
+background-color: #d9cdcd;
+font-family: Open Sans,Helvetica, Arial, Sans-Serif, serif;
+font-weight: 600;
+font-size: 12px;
+line-height: 1;
+letter-spacing: 1px;
+text-decoration: none;
+text-transform: uppercase;
+color: rgba(var(--kubio-color-6-variant-4),1);
+border-top-color: rgba(217, 205, 205, 0.7);
+border-top-width: 2px;
+border-top-style: solid;
+border-right-color: rgba(217, 205, 205, 0.7);
+border-right-width: 2px;
+border-right-style: solid;
+border-bottom-color: rgba(217, 205, 205, 0.7);
+border-bottom-width: 2px;
+border-bottom-style: solid;
+border-left-color: rgba(217, 205, 205, 0.7);
+border-left-width: 2px;
+border-left-style: solid;
+border-top-left-radius: 5px;
+border-top-right-radius: 5px;
+border-bottom-left-radius: 5px;
+border-bottom-right-radius: 5px;
+padding-top: 12px;
+padding-bottom: 12px;
+padding-left: 24px;
+padding-right: 24px;
+justify-content: center;\" href=\"/account?type=courses&autooptin=$autooptinnew\">$autooptinbutton</a><br><br>";
         echo "<div class=\"courses\" style=\"display: grid; grid-template-columns: repeat($columnsNeeded, 1fr); column-gap: 1rem; row-gap: 1rem; grid-template-rows: repeat($rowsNeeded, 1fr);\">";
         foreach ($registeredCourses as $registeredCourse) {
             if ($registeredCourse == null or $registeredCourse == "" or $registeredCourse < 1) {
@@ -66,6 +120,7 @@ function uwp_account_form_display_cb($type)
             $course = $wpdb->get_row("SELECT * FROM $wpdb->prefix" . "courses WHERE ID = " . $registeredCourse);
             $cDate = new DateTime($course->date);
             $courseDate = $cDate->format('d.m.Y H:i');
+
             echo "<div class=\"course\" style=\"grid-column: $column; grid-row: $row; \">
             <div id=\"course$course->id\" class=\"course-card\">
         <div class=\"card-body\">
@@ -74,6 +129,7 @@ function uwp_account_form_display_cb($type)
                     </u></b></h5>
             <p class=\"card-text\">$course->description</p>
             <p><b>Datum:</b> $courseDate</p>
+            <p><a href=\"$course->url\">Link zum Kurs</a></p>
         </div>
     </div></a>
 </div>
