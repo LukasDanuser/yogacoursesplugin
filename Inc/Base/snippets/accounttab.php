83ff2604b6c502df697162495f4306402321626a
Lukas Danuser <lukas.danuser@gmail.com>
2023-06-13 15:01:10 +0200

    updated snipped so it rewrites account page layout

--- Inc/Base/snippets/accounttab.php
+++ Inc/Base/snippets/accounttab.php
@@ -15,6 +15,10 @@ function uwp_account_available_tabs_cb($tabs)
         'title' => __('Meine Kurse', 'userswp'),
         'icon'  => 'fas fa-graduation-cap',
     );
+    $tabs['videos'] = array(
+        'title' => __('Videos', 'userswp'),
+        'icon'  => 'fas fa-video mr-1 fa-fw',
+    );
     $tabs['change-password'] = array(
         'title' => __('Passwort ändern', 'userswp'),
         'icon'  => 'fas fa-asterisk mr-1 fa-fw',
@@ -27,6 +31,9 @@ function uwp_account_page_title_cb($title, $type)
     if ($type == 'courses') {
         $title = __('Meine Kurse', 'uwp-messaging');
     }
+    if ($type == 'videos') {
+        $title = __('Videos', 'uwp-messaging');
+    }
 
     return $title;
 }
@@ -35,6 +42,15 @@ add_filter('uwp_account_form_display', 'uwp_account_form_display_cb', 10, 1);
 function uwp_account_form_display_cb($type)
 {
     global $wpdb;
+    $current_user = wp_get_current_user();
+    $display_name = $current_user->display_name;
+    echo "<style> .card-body {display: none !important;}
+                  .mt-0:hover {color: black !important;} </style>";
+    echo "<script> 
+    var element = document.querySelectorAll('a.mt-0');
+    element[0].innerHTML = \"$display_name\";
+    element[0].removeAttribute('href');
+    element[0].removeAttribute('title');</script>";
     if ($type == 'courses') {
 
         $plugin_data = get_plugin_data(dirname(__FILE__, 3) . '/coursesplugin/courses-plugin.php');
@@ -137,4 +153,70 @@ justify-content: center;\" href=\"/account?type=courses&autooptin=$autooptinnew\
         }
         echo "</div>";
     }
+
+    if ($type == 'videos') {
+        $plugin_data = get_plugin_data(dirname(__FILE__, 3) . '/coursesplugin/courses-plugin.php');
+        $plugin_version = $plugin_data['Version'];
+        wp_enqueue_style('style', '/wp-content/plugins/coursesplugin/assets/style.css', __FILE__, $plugin_version);
+
+        if (is_user_logged_in()) {
+            $userID = get_current_user_id();
+            $membership = $wpdb->get_var("SELECT membership FROM $wpdb->prefix" . "users WHERE ID = $userID");
+            $valid_until = $wpdb->get_var("SELECT subscription_valid_until FROM $wpdb->prefix" . "users WHERE ID = $userID");
+            $date = date("Y-m-d");
+            if ($valid_until != "0000-00-00" and $membership != "0") {
+                if ($date > $valid_until) {
+                    echo "<div class=\"accessDenied\" style=\"text-align: center;\">";
+                    echo "<p style=\"border: 1px solid orange;max-width: fit-content;padding: 0.3rem;margin: auto;border-radius: 0.2rem;\">Diese Seite ist nur für Mitglieder zugänglich!<br>Sie können <a href=\"/membership\">hier</a> eine Mitgliedschaft kaufen.</p>";
+                    echo "</div>";
+                } else {
+                    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                    $videos = $wpdb->get_results("SELECT * FROM $wpdb->prefix" . "courseVideos");
+                    $rowsNeeded = ceil(sizeof($videos) / 4);
+                    $courseAmount = sizeof($videos);
+                    $columnsNeeded = $courseAmount >= 4 ? 4 : 3;
+                    $columnsNeeded = $courseAmount == 2 ? 2 : $columnsNeeded;
+                    $centerThree = $courseAmount == 1 ? true : false;
+                    $maxWidth = ($courseAmount == 2) ? "50%" : (($courseAmount == 3)  ? "75%" : "100%");
+                    if (!empty($videos)) {
+                        $t1 = 0;
+                        $t2 = 1;
+                        echo "<style>.wp-container-7>* {
+                    max-width: $maxWidth !important;
+                }</style>";
+                        echo "<div class=\"videos\" style=\"display: grid; grid-template-columns: repeat($columnsNeeded, 1fr); column-gap: 1rem; row-gap: 1rem; grid-template-rows: repeat($rowsNeeded, 1fr);\">";
+                        foreach ($videos as $video) {
+                            $t1++;
+                            if ($t1 > $columnsNeeded) {
+                                $t2++;
+                                $t1 = 1;
+                            }
+                            $t1 = $centerThree == true ? 2 : $t1;
+                            echo "
+                    <div class=\"video\" style=\"grid-column: $t1; grid-row: $t2; margin-left: auto !important; margin-right: auto !important; max-width: 30rem !important;\">
+                        <div id=\"video$video->id\" class=\"video-card\">
+                            <div class=\"video-body\">
+                                <h5 class=\"video-title text-center\"><b><u>
+                                 <p>$video->video_name</p>
+                                </u></b></h5>
+                                <p class=\"video-text\">$video->video_description</p>
+                                <video width=\"320\" height=\"240\" controls>
+                                    <source src=\"$video->file_url\" type=\"video/mp4\">
+                                    Your browser does not support the video tag.
+                                </video> <br>
+                            </div>
+                        </div>
+                    </div>";
+                        }
+                        echo "</div>";
+                    }
+                }
+            } else {
+                echo "<div class=\"accessDenied\" style=\"text-align: center;\">";
+                echo "<p style=\"border: 1px solid orange;max-width: fit-content;padding: 0.3rem;margin: auto;border-radius: 0.2rem;\">Diese Seite ist nur für Mitglieder zugänglich!<br>Sie können <a href=\"/mitgliedschaft\">hier</a> eine Mitgliedschaft kaufen.</p>";
+                echo "</div>";
+            }
+        }
+    }
+
 }
